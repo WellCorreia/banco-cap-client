318d74130257734dda6615e2ede6feac2e37bce9
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-04-18 00:15:30 -0300

    Configurado mensagens de erro.

--- src/features/conta/store/actions.js
+++ src/features/conta/store/actions.js
@@ -5,11 +5,15 @@ import * as types from './mutation-types'
 export const ActionGetSaldo = ({ commit }, payload) => {
   return services.conta.getSaldo(payload).then(response => {
     commit(types.SET_SALDO, response.data)
+  }).catch(error => {
+    commit(types.SET_SALDO, error.data)
   });
 }
 
 export const ActionCriarConta = ({ commit }, payload) => {
   return services.conta.createConta(payload).then(response => {
     commit(types.SET_CONTA, response.data)
+  }).catch(error => {
+    commit(types.SET_CONTA, error.data)
   });
 }
--- src/features/operacao/store/actions.js
+++ src/features/operacao/store/actions.js
@@ -5,5 +5,7 @@ import * as types from './mutation-types'
 export const ActionDepositarSacar = ({ commit }, payload) => {
   return services.conta.createTransaction(payload).then(response => {
     commit(types.SET_OPERACAO, response.data)
+  }).catch(error => {
+    commit(types.SET_OPERACAO, error.data)
   });
 }
