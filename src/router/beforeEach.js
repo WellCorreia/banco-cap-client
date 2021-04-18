ff6c2cf87089847b1e9fc8b5acefde212cfc9a05
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-04-18 00:31:20 -0300

    Corrigido redirecionamento de página.

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -1,9 +1,7 @@
-import store from '../store';
-
 export default async (to, from, next) => {
-  if (to.path === '/') {
+  if (to.path === '/' || to.name === null) {
     next({ name: 'saldo' });
   } else {
-    next({ path: to.path });
+    next();
   }
 };
